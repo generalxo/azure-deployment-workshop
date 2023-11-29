ce2f306cf856b7881ecaa0c07facd5acbd7bd9dd
Oliver Wasner <oli.wasner@hotmail.com>
2023-11-29 03:03:24 +0100

    first commit

---
+++ net22-azure-deploy-ws/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using net22_azure_deploy_ws.Models;
+using System.Diagnostics;
+
+namespace net22_azure_deploy_ws.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _config;
+        public HomeController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.MyGreeting = _config["MyGreeting"];
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ net22-azure-deploy-ws/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace net22_azure_deploy_ws.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
